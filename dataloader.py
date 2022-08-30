61fbcbbe85ac5b44e6c83ffeb362b34b2def2bd0
naparicioc <n.aparicioc@uniandes.edu.co>
2022-08-30 10:10:20 -0500

    Dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,19 +1,26 @@
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 import glob
+import os
 
 class Burned(Dataset):
 	def __init__(self, path, transform_img, transform_target):
 		split = path.split('/')[-1]
 		self.images = glob.glob(os.path.join(split, '*.jpg'))
-		self.labels = glob.glob(os.path.join(split, '*.txt'))
+		self.transform_img = transform_img
+		self.transform_target = transform_target
+		#self.labels = glob.glob(os.path.join(split, '*.txt')) Solo utillizaremos las imágenes
 
 	def __getitem__(self, index):
 		image = read_image(self.images[index])
-		label_file = self.labels[index]
+		image = self.transform_img(image)
+		#file_exists = os.path.exists(self.images[index].replace('.jpg', '.txt'))
+		#if file_exists:
+		label_file = self.images[index].replace('.jpg', '.txt')
 		with open(label_file) as f:
 			info_annotation = f.readlines()
 		label = int(info_annotation[0].split(' ')[0])
+		label = self.transform_target(label)
 		return image, label
 
 	def __len__(self):
