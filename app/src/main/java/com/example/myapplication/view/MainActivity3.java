5767f362ef1e60557b9ce729ec2bf7ef0875cd33
Ersebreck <81669194+Ersebreck@users.noreply.github.com>
2022-10-10 03:34:27 -0500

    Avance

--- app/src/main/java/com/example/myapplication/view/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/view/MainActivity3.java
@@ -1,7 +1,9 @@
 package com.example.myapplication.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.myapplication.R;
@@ -17,5 +19,13 @@ public class MainActivity3 extends AppCompatActivity {
 
         TextView textview3 = findViewById(R.id.resultado);
         textview3.setText(String.valueOf(num1));
+
+        findViewById(R.id.regresar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent testIntent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(testIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/myapplication/view/MainActivityH.java
@@ -0,0 +1,33 @@
+package com.example.myapplication.view;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.myapplication.R;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivityH extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_h);
+    }
+
+    /**private void cargarPreferencias(){
+        SharedPreferences usuarios=getSharedPreferences
+                ("usuarios",MODE_PRIVATE);
+
+        String cedula = usuarios.getString("cedula", "No existe este usuario");
+
+        SharedPreferences usuario=getSharedPreferences
+                (cedula, MODE_PRIVATE);
+
+        String name = usuario.getString("nombre","xd");
+        String age = usuario.getString("nombre","xd");
+        String mail = usuario.getString("nombre","xd");
+        String cirugia = usuario.getString("nombre","xd");
+        String eps = usuario.getString("nombre","xd");
+    }*/
+}
