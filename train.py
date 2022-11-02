01b7bb51f9fc3efc85f4a86ccdda68907ae99599
Nicolas Aparicio <naparicioc@lambda002.uniandes.edu.co>
2022-11-01 23:59:05 -0400

    Train

--- train.py
+++ train.py
@@ -4,14 +4,15 @@ import time
 import argparse
 import os.path as osp
 
+import copy
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision import datasets, transforms
 
-from dataloader import Burned
-from model import AlexNet
+from dataset import Burned
+from alexnet_pytorch import AlexNet
 
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
@@ -43,26 +44,22 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
-
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
-train_loader = torch.utils.data.DataLoader(
-    Burned('../train', transform_img=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))]
-                   )),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
+train_loader = torch.utils.data.DataLoader(Burned('Burned_Data/train', transform_img=transforms.Compose([transforms.ToPILImage(),
+                transforms.Resize((256,256)),
+                transforms.ToTensor()]), 
+                       transform_target=None), 
+                       batch_size=args.batch_size, shuffle=True, **kwargs)
 
+test_loader = torch.utils.data.DataLoader(Burned('Burned_Data/test', transform_img=transforms.Compose([transforms.ToPILImage(),
+                transforms.Resize((256,256)),
+                transforms.ToTensor()]), 
+                       transform_target=None), 
+                       batch_size=args.batch_size, shuffle=True, **kwargs)
 
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))]
-                   )),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-
-model = Net()
+model = AlexNet.from_pretrained('alexnet', num_classes=3)
+cp_model = copy.deepcopy(model)
 
 if args.cuda:
     model.cuda()
@@ -75,7 +72,6 @@ if osp.exists(args.save):
         load_model = True
 
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-
 
 def train(epoch):
     model.train()
@@ -91,7 +87,7 @@ def train(epoch):
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+                100. * batch_idx / len(train_loader), loss.data))
 
 
 def test(epoch):
@@ -103,7 +99,7 @@ def test(epoch):
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
-        test_loss += F.nll_loss(output, target).data[0]
+        test_loss += F.nll_loss(output, target).data
         # get the index of the max log-probability
         pred = output.data.max(1)[1]
         correct += pred.eq(target.data).cpu().sum()
